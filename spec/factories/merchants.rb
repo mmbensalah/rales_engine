5c32ec025e7bce07b3efe22928900f7d317b83c2
mmbensalah <mmbensalah@gmail.com>
2018-10-02 22:48:25 -0600

    creates index and show for merchant api

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
-  factory :customer do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
+  factory :merchant do
+    name { Faker::Name.name }
     created_at { "2018-10-02 19:21:50" }
     updated_at { "2018-10-02 19:21:50" }
   end
