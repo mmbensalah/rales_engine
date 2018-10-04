7b5331d0de38bb936813413671d6a83d7a8fc55a
mmbensalah <mmbensalah@gmail.com>
2018-10-04 12:31:26 -0600

    ADDS index and show to invoice_item and transaction controller

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,36 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
+
+  def self.top_merchants(number)
+    unscoped.select("merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue")
+	  .joins("INNER JOIN invoices ON invoices.merchant_id = merchants.id
+            INNER JOIN invoice_items ON invoice_items.invoice_id = invoices.id")
+	  .limit(number)
+	  .group(:id)
+	  .order("revenue DESC")
+  end
+
+  def self.top_merchants_by_items(number)
+    unscoped.select("merchants.*, SUM(invoice_items.quantity) AS total_items")
+	  .joins("INNER JOIN invoices ON invoices.merchant_id = merchants.id
+		        INNER JOIN invoice_items ON invoice_items.invoice_id = invoices.id")
+	  .limit(number)
+	  .group(:id)
+	  .order("total_items DESC")
+  end
+
+  def self.revenue_by_date(date)
+    Merchant.unscoped
+    .select("merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS days_revenue")
+    .joins("INNER JOIN invoices ON invoices.merchant_id = merchants.id
+            INNER JOIN invoice_items ON invoice_items.invoice_id = invoices.id")
+    .where("invoice_items.created_at between 'date' and 'date 23:59:59'")
+    .group(:id)
+    .order("days_revenue DESC")
+  end
+
+
+
+
 end
